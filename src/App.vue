0fca8164cba6adcb006e360634dc9d9448627a64
A22000055IsmailFawwazAlHasyir <a2.2000055@mhs.stmik-sumedang.ac.id>
2024-10-29 22:47:56 +0700

    fix: build

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
-import { useDialogStore } from './stores/dialog'
-import { ActionDialog } from './components/dialogs'
+	import { RouterView } from "vue-router";
+	import { useDialogStore } from "./stores/dialog";
+	import { ActionDialog } from "./components/dialogs";
 
-const dialog = useDialogStore()
+	const dialog = useDialogStore();
 
-const onClose = () => {
-  dialog.open = false
-}
+	const onClose = () => {
+		dialog.open = false;
+	};
 </script>
 
 <template>
-  <RouterView />
-  <ActionDialog
-    :show-cancel-button="dialog.showCancelButton"
-    :dismiss-on-action="dialog.dismissOnAction"
-    :open="dialog.open"
-    :title="dialog.title"
-    :message="dialog.content"
-    :confirm-text="dialog.confirmText"
-    :cancel-text="dialog.cancelText"
-    @on-close="onClose"
-    @on-confirm="dialog.onConfirm"
-    @on-cancel="dialog.onCancel"
-    :type="dialog.type"
-  />
+	<RouterView />
+	<ActionDialog
+		:show-cancel-button="dialog.showCancelButton"
+		:dismiss-on-action="dialog.dismissOnAction"
+		:open="dialog.open"
+		:title="dialog.title"
+		:message="dialog.content"
+		:confirm-text="dialog.confirmText"
+		:cancel-text="dialog.cancelText"
+		@on-close="onClose"
+		:type="dialog.type"
+	/>
 </template>
